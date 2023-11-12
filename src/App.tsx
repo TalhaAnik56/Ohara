487a2f9c9f6cd6236476d2061db0a998c176926f
TalhaAnik56 <aniktalha@gmail.com>
2023-11-13 01:51:07 +0600

    installed Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
+      <Button colorScheme="blue">Checking</Button>
     </>
   );
 };
